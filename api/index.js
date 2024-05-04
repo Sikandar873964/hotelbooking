f51a9e79e721e998783bdfbd592214a6660af63f
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-04 16:23:44 +0530

    starter template for backend

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
+
+
+app.listen(8800, () => {
+    console.log("Connected to backend.");
+    console.log("Backend Running on 8800");
+
+  });
