70793bc95506f1e932d46a9aafa6a5e57594a178
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-04 18:21:14 +0530

    added auth context provider in index.js

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import {DarkModeContextProvider} from "./context/darkModeContext"
+import { AuthContextProvider } from "./context/AuthContext";
+
 
 
 ReactDOM.render(
-    <DarkModeContextProvider>
-    <App />
-  </DarkModeContextProvider>
+    <AuthContextProvider>
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </AuthContextProvider>
       ,
   document.getElementById("root")
 );
