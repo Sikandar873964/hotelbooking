7059dc93948400420c4c678f618ba6bb32c11c5e
Sikandar873964 <873964@stu.ucenmanchester.ac.uk>
2024-05-04 16:26:12 +0530

    added database connectiom

--- api/index.js
+++ api/index.js
@@ -1,10 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
+
+
 const app = express();
 dotenv.config();
 
 
+//Database connection (MONGO DB)
+const connect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO);
+      console.log("Connected to mongoDB.");
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+  });
+
+
+// main app run  
 app.listen(8800, () => {
+    connect();
     console.log("Connected to backend.");
     console.log("Backend Running on 8800");
 
