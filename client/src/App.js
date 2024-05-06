30315d59831b54829fbc831a226d61ba2a393dbb
Sikandar <sikandarbutt@hotmail.co.uk>
2024-05-06 17:18:37 +0100

    added hotels route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+
 
 
 function App() {
@@ -7,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<List />} />
       </Routes>
     </BrowserRouter>
   );
