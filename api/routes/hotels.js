2b4d26885147f78d23a9b785bc72d7506aa3f1b0
Sikandar <sikandarbutt@hotmail.co.uk>
2024-05-06 15:57:54 +0100

    create hotels end point added

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {
-  getHotels
+  getHotels,
+  createHotel
 } from "../controllers/hotel.js";
 const router = express.Router();
 
 // get all htels
 router.get("/", getHotels);
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
 
 export default router;
